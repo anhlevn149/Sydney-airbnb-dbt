aa9dd7656584e0c012030231ba19e25377737f9c
Anh Le <vungocanh.le@student.uts.edu.au>
2023-10-31 01:47:07 +1100

    dm_host_neighbourhood

--- models/datamart/dm_host_neighbourhood.sql
+++ models/datamart/dm_host_neighbourhood.sql
@@ -1,1 +1,17 @@
+select 
+	*,
+	(total_revenue / distinct_hosts) as revenue_per_host
+from 
+(select 
+	host_neighbourhood_lga_name,
+	to_date(concat(extract(month from scraped_date)
+		   , '-'
+		   , extract(year from scraped_date)), 'MM/YYYY') as month_year,
+	count(distinct host_id) as distinct_hosts,
+	sum(price * (30 - case when has_availability='t' then availability_30 end)) as total_revenue
+from warehouse.facts_listings
+group by host_neighbourhood_lga_name, month_year
+order by host_neighbourhood_lga_name, month_year
+) foo
+
 
