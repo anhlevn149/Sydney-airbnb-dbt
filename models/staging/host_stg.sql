28b21cbf36f77e0d2e67a8cf12ad1ba06e1d2127
Anh Le <vungocanh.le@student.uts.edu.au>
2023-11-02 12:18:47 +1100

    update host_stg.sql with UNKNOWN values for host_neighbourhood

--- models/staging/host_stg.sql
+++ models/staging/host_stg.sql
@@ -19,7 +19,7 @@ cleaned as (
         case when host_name is not null then host_name else 'unknown' end as host_name,
         case when host_since ~ '^[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}$' then to_date(host_since, 'DD/MM/YYYY') else null::date end as host_since,
         case when host_is_superhost is not null then host_is_superhost else 'unknown' end as host_is_superhost,
-        upper(case when host_neighbourhood is not null then host_neighbourhood else '' end) as host_neighbourhood,
+        upper(case when host_neighbourhood is not null then host_neighbourhood else 'unknown' end) as host_neighbourhood,
         case when dbt_valid_from = (select min(dbt_valid_from) from source) then '1900-01-01'::date else dbt_valid_from::date end as dbt_valid_from,
         dbt_valid_to::date
     from source
